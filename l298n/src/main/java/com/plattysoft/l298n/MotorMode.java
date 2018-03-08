7bae431b348736e28599b8821a0949961d0a5e8f
plattysoft <raul@plattysoft.com>
2018-03-08 22:10:27 +0000

    added driver for l298n motor controller

---
+++ l298n/src/main/java/com/plattysoft/l298n/MotorMode.java
@@ -0,0 +1,9 @@
+package com.plattysoft.l298n;
+
+/**
+ * Created by Raul Portales on 16/12/17.
+ */
+
+public enum MotorMode {
+    BACKWARD, TURN_RIGHT, TURN_LEFT, SPIN_RIGHT, SPIN_LEFT, STOP, FORWARD
+}
