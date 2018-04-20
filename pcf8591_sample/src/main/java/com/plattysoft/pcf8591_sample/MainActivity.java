df27b69cc3255c13d23e757ef47952b8151a5925
plattysoft <raul@plattysoft.com>
2018-04-20 21:20:41 +0100

    added missing import

--- pcf8591_sample/src/main/java/com/plattysoft/pcf8591_sample/MainActivity.java
+++ pcf8591_sample/src/main/java/com/plattysoft/pcf8591_sample/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.google.android.things.contrib.driver.apa102.Apa102;
 import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay;
+import com.google.android.things.contrib.driver.ht16k33.Ht16k33;
 import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.GpioCallback;
